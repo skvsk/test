2bc752f31f948d50ced0f74bd39b5361b748c2d2
Alex Saikovsky <a.saikovsky@gmail.com>
2011-07-24 13:32:22 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gogo::Application.config.secret_token = '269af1751cb2f5ba5319f9c1c34aacb23e33340df3627f7c24cf3d24295b7f4306f8c552409c97dea159d94b3fe2305155530992a337aef1fe3fdece284d92ea'
